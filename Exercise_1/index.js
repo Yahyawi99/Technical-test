76e134506d186e36884d68e23c3485ef77f64e51
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-08 19:18:05 +0100

    t

--- Exercise_1/index.js
+++ Exercise_1/index.js
@@ -3,12 +3,14 @@ const output = [];
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
     output.push("Hello,");
-  } else if (i % 5 === 0) {
+  }
+
+  if (i % 5 === 0) {
     output.push("World,");
-  } else if (i % 7 === 0) {
-    output.push("Yoo,");
-  } else {
-    output.push(i + ",");
+  }
+
+  if (i % 7 === 0) {
+    output.push("Yoo, ");
   }
 }
 
